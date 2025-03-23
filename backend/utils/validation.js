bee83fd7159be5e1ac6a11cb3edef359ab787233
SeymaMolnar <seyma.ercan@gmail.com>
2025-03-22 22:27:11 -0400

    delpoyment chnages

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,4 +1,4 @@
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
 
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
@@ -9,13 +9,13 @@ const handleValidationErrors = (req, _res, next) => {
     const errors = {};
     validationErrors
       .array()
-      .forEach(error => errors[error.path] = error.msg);
+      .forEach((error) => (errors[error.path] = error.msg));
 
     const err = Error("Bad request.");
     err.errors = errors;
     err.status = 400;
     err.title = "Bad request.";
-    next(err);
+    return next(err);
   }
   next();
 };
@@ -27,18 +27,18 @@ const handleValidationErrors403 = (req, _res, next) => {
     const errors = {};
     validationErrors
       .array()
-      .forEach(error => errors[error.path] = error.msg);
+      .forEach((error) => (errors[error.path] = error.msg));
 
     const err = Error("Bad request.");
     err.errors = errors;
     err.status = 403;
     err.title = "Bad request.";
-    next(err);
+    return next(err);
   }
   next();
 };
 
 module.exports = {
   handleValidationErrors,
-  handleValidationErrors403
+  handleValidationErrors403,
 };
